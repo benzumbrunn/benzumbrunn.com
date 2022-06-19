ef376e8507d85af2e1f21a5ec7e06fecc3b01dab
Ben Zumbrunn <benzumbrunn@gmail.com>
2022-06-19 22:29:54 +0800

    Add links, change font, add favicon

---
+++ components/HomeLink/HomeLink.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './HomeLink.module.css'
+
+export default function HomeLink(props) {
+  return (
+    <div className={styles.component}>
+      <a href={props.link}>
+        <Image
+          className={styles.image}
+          src={props.img}
+          width="80" height="80" />
+      </a>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import HomeLink from '../components/HomeLink/HomeLink'
 
 export default function Home() {
   return (
@@ -7,12 +8,29 @@ export default function Home() {
       <Head>
         <title>benzumbrunn.com</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          benzumbrunn.com
+          Ben Zumbrunn
         </h1>
+        <div className={styles.links}>
+          <HomeLink
+            link="https://twitter.com/benzumbrunn"
+            img="/../public/images/twitter.png"
+          />
+          <HomeLink
+            link="https://github.com/benzumbrunn"
+            img="/../public/images/github.png"
+          />
+          <HomeLink
+            link="https://t.me/benzumbrunn"
+            img="/../public/images/telegram.png"
+          />
+        </div>
       </main>
 
       <footer className={styles.footer}>
