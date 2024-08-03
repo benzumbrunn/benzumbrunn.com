f2c11d9b55dd3b4deaa2aad354b10bfd474b72ab
Ben Zumbrunn <benzumbrunn@gmail.com>
2024-08-03 14:15:05 +0800

    Replace GTM with Plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,9 @@
 import '../styles/globals.css'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-C2G48QTB4Y');
-          `,
-        }}
-      />
+      <script defer data-domain="benzumbrunn.com" src="https://plausible.io/js/script.js"></script>
       <Component {...pageProps} />
     </>
   )
